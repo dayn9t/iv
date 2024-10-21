d41d5bb5598a1b3838e3dfc30f72b3fd2d010ccb
dayn9t <dayn9t@gmail.com>
2024-10-21 13:29:10 +0800

    24-10-21

--- iv-mm/src/bin/exif-io.rs
+++ iv-mm/src/bin/exif-io.rs
@@ -1,8 +1,12 @@
+use rexiv2::GpsInfo;
+use rx_core::time::now;
 use iv_mm::meta::ExifManager;
 
 fn main() {
-    let file = "/home/jiang/rs/iv/data/image/jack2.jpg";
-    let exif = ExifManager::new(file).unwrap();
+    //let file = "/home/jiang/rs/iv/data/image/jack2.jpg";
+    let file = "/home/jiang/1/s4/0001.jpg";
+    let dst = "/home/jiang/1/s4/0001a.jpg";
+    let mut exif = ExifManager::new(file).unwrap();
 
     let size = exif.get_size();
     println!("Size: {size}");
@@ -14,4 +18,16 @@ fn main() {
     if let Some(time) = exif.get_time() {
         println!("Time: {time}");
     }
+
+    let gps = GpsInfo {
+        latitude: 37.0,
+        longitude: -122.0,
+        altitude: 0.0,
+    };
+    exif.set_gps_coordinates(gps).unwrap();
+
+    let time = now();
+    exif.set_time(time).unwrap();
+
+    exif.save(file).unwrap();
 }
